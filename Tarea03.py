74aef6427f627a1edd43f3722aec3861753db50d
Jorge Gacitua <jorge.gacitua@dgf.uchile.cl>
2015-10-03 23:49:07 -0300

    se implementa el problema 1, pero existe un problema de overflow

--- Tarea03.py
+++ Tarea03.py
@@ -3,7 +3,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-mu=1.560 #rut=17471560-2
+
 
 def get_k1(f,h,g,g_prima,xi):
     '''
@@ -20,7 +20,7 @@ def get_k2(f,h,g,g_prima,xi):
     entrega el verctor k1 para utilizar en el metodo Runge Kutta 3
     '''
     k1 = get_k1(f,h,g,g_prima,xi)
-    f_eval = f(xi+h/2,phi_n + k1[0]/2, w_n + k1[1]/2)
+    f_eval = f(xi+h/2,g + k1[0]/2, g_prima + k1[1]/2)
     return h * f_eval[0], h * f_eval[1]
 
 def get_k3(f,h,g,g_prima,xi):
@@ -30,7 +30,7 @@ def get_k3(f,h,g,g_prima,xi):
     '''
     k1 = get_k1(f,h,g,g_prima,xi)
     k2 = get_k2(f,h,g,g_prima,xi)
-    f_eval = f(xi+h,phi_n + k1[0]+2*k2[0], w_n + k1[0]+2*k2[0])
+    f_eval = f(xi+h,g - k1[0]+2*k2[0], g_prima - k1[0]+2*k2[0])
     return h * f_eval[0], h * f_eval[1]
 
 def RK3(f,h,g,g_prima,xi):
@@ -42,6 +42,48 @@ def RK3(f,h,g,g_prima,xi):
     k2 = get_k2(f,h,g,g_prima,xi)
     k3 = get_k1(f,h,g,g_prima,xi)
 
-    Y1n1 = g + (k1[0]+4*k2[0]+k3[0])/6
-    Y2n1 = g_prima + (k1[1]+4*k2[1]+k3[1])/6
-    return phi_n1, w_n1
+    Y1n1 = g + (k1[0]+4*k2[0]+k3[0])/6.0
+    Y2n1 = g_prima + (k1[1]+4*k2[1]+k3[1])/6.0
+    return Y1n1,Y2n1
+
+#----------------------------------Problema 1----------------------------------#
+mu=1.560 #rut=17471560-2
+N_steps = 10000
+dyds=np.zeros(N_steps)
+y=np.zeros(N_steps)
+h=(20*np.pi)/N_steps
+
+def f(xi,g,g_prima):
+    f1=g_prima
+    f2=-g-mu*((g**2)-1)*g_prima
+    return f1,f2
+
+dyds[0]=0
+y[0]=4.0
+
+for i in range(1, N_steps):
+    y[i], dyds[i]=RK3(f,h,y[i-1],dyds[i-1],0)
+
+
+s=[h * i for i in range(N_steps)]
+
+
+plt.figure(1)
+plt.clf()
+
+plt.plot(s,y)
+
+plt.xlabel('s ()')
+plt.ylabel('$y(s)$', fontsize=18)
+plt.show()
+plt.draw()
+
+plt.figure(2)
+plt.clf()
+
+plt.plot(y,dyds)
+
+plt.xlabel('y(s)',fontsize=18)
+plt.ylabel('$dy/ds$', fontsize=18)
+plt.show()
+plt.draw()
