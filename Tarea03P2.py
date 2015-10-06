8e2f3cc2bc92333e218d6c5c5f207ca53ecc7f86
Jorge Gacitua <jorge.gacitua@dgf.uchile.cl>
2015-10-05 23:33:11 -0300

    Se termina de implementar lel Problema 2

--- Tarea03P2.py
+++ Tarea03P2.py
@@ -2,38 +2,13 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+
+from scipy.integrate import ode
+
 from mpl_toolkits.mplot3d import Axes3D
 
-#Fuente: http://www.codeproject.com/Tips/792927/Fourth-Order-Runge-Kutta-Method-in-Python#
-def rKN(x, fx, n, hs):
-    k1 = []
-    k2 = []
-    k3 = []
-    k4 = []
-    xk = []
-    for i in range(n):
-        k1.append(fx[i](x)*hs)
-    for i in range(n):
-        xk.append(x[i] + k1[i]*0.5)
-    for i in range(n):
-        k2.append(fx[i](xk)*hs)
-    for i in range(n):
-        xk[i] = x[i] + k2[i]*0.5
-    for i in range(n):
-        k3.append(fx[i](xk)*hs)
-    for i in range(n):
-        xk[i] = x[i] + k3[i]
-    for i in range(n):
-        k4.append(fx[i](xk)*hs)
-    for i in range(n):
-        x[i] = x[i] + (k1[i] + 2*(k2[i] + k3[i]) + k4[i])/6
-    return x
-
-#------------------------Se definen las funiones a resolver--------------------#
-
-def dxdt(k):return sigma*(k[1]-k[0])
-def dydt(k):return k[0]*(rho-k[2])-k[1]
-def dzdt(k):return k[0]*k[1]-beta*k[2]
+def Lorenz_system(t,k):
+    return [sigma*(k[1]-k[0]), k[0]*(rho-k[2])-k[1], k[0]*k[1]-beta*k[2]]
 
 sigma=10
 beta=8/3
@@ -43,16 +18,17 @@ N_steps = 10000
 x=np.zeros(N_steps)
 y=np.zeros(N_steps)
 z=np.zeros(N_steps)
-h=(100) / N_steps
-x[0]=1
-y[0]=1
-z[0]=1
-f=[dxdt,dydt,dzdt]
+xyzi=[1,1,1]
+t_values = np.linspace(1e-3, 30, N_steps)
 
-for i in range(1, N_steps):
-    xyz=[x[i-1],y[i-1],z[i-1]]
-    x[i], y[i],z[i]=rKN(xyz,f,3,h)
+r = ode(Lorenz_system)
+r.set_integrator('dopri5')
+r.set_initial_value(xyzi)
 
+
+for i in range(N_steps):
+    r.integrate(t_values[i])
+    x[i], y[i], z[i] = r.y
 
 #------------------------ se crea el grafico----------------------------------#
 
@@ -62,8 +38,6 @@ fig.clf()
 
 ax = fig.add_subplot(111, projection='3d')
 ax.set_aspect('equal')
-
-t = [h * i for i in range(N_steps)]
 
 
 ax.plot(x, y, z)
